9c93e272c4cddb9f2ff1c9dcec508311ea80c4ec
Nikolai <rivennike@gmail.com>
2019-04-02 11:30:26 -0700

    Menu button, JS and rotate added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,68 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 const ListLink = props => (
-  <li className="nav-item">
+  <li className={"nav-item " + props.menuClass}>
     <Link to={props.to} className="nav-link">
       {props.children}
     </Link>
   </li>
 )
 
-const Header = () => (
-  <header>
-    <div className="menu-btn">
-      <div className="btn-line" />
-      <div className="btn-line" />
-      <div className="btn-line" />
-    </div>
+class Header extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showMenu: false,
+      btnClass: 'close',
+      menuClass: 'show'
+    }
+  }
 
-    <nav className="menu">
-      <div className="menu-branding">
-        <div className="portrait" />
-      </div>
-      <ul className="menu-nav">
-        <ListLink to="/">HOME</ListLink>
-        <ListLink to="/about/">ABOUT ME</ListLink>
-        <ListLink to="/work/">MY WORK</ListLink>
-        <ListLink to="/contact/">CONTACT ME</ListLink>
-      </ul>
-    </nav>
-  </header>
-)
+  toggleMenu = () => {
+    console.log('Toggled')
+    this.setState((prevState) => {
+      console.log(`${prevState} becomes `)
+      if (!prevState.showMenu) {
+        return {
+          showMenu: true,
+          btnClass: '',
+          menuClass: ''
+          }
+        }
+        else {
+          return {
+          showMenu: false,
+          btnClass: 'close',
+          menuClass: 'show'
+          }
+        }
+    })
+    console.log(this.state)
+  }
 
+  render() { 
+    return (
+      <header>
+        <div className={'menu-btn ' + this.state.btnClass} onClick={this.toggleMenu}>
+          <div className="btn-line" />
+          <div className="btn-line" />
+          <div className="btn-line" />
+        </div>
+
+        <nav className={"menu " +  this.state.menuClass}>
+          <div className={"menu-branding " +  this.state.menuClass}>
+            <div className="portrait" />
+          </div>
+          <ul className={"menu-nav " +  this.state.menuClass}>
+            <ListLink to="/" menuClass={this.state.menuClass}>HOME</ListLink>
+            <ListLink to="/about/" menuClass={this.state.menuClass}>ABOUT ME</ListLink>
+            <ListLink to="/work/" menuClass={this.state.menuClass}>MY WORK</ListLink>
+            <ListLink to="/contact/" menuClass={this.state.menuClass}>CONTACT ME</ListLink>
+          </ul>
+        </nav>
+    </header>
+    )
+  }
+}
 export default Header
